631853fd637f885c4e4ac376884f0788e117ebe1
nccaster <ncapara@miuandes.cl>
2019-10-16 18:39:41 -0300

    resumen y update a page fault handler

--- main.c
+++ main.c
@@ -15,15 +15,32 @@ how to use the page table and disk interfaces.
 #include <string.h>
 #include <errno.h>
 
-typedef struct frameArray//contiene el arreglo de marco
+//Contadores para imprimir resultado
+int n_faltas_de_pagina = 0, n_lecturas = 0, n_escrituras = 0;
+//valor de argv[3]
+char *metodo;
+//contiene el arreglo de marco
+typedef struct frameArray
 {
 	int *array;
 	int length;
 }frameArray;
-frameArray *createframeArray(int largo);//funcion para crear arreglo de marcos
+
+frameArray *createframeArray(int largo);//funcion para crear arreglo simple de marcos
+
 void page_fault_handler( struct page_table *pt, int page )
 {
 	printf("page fault on page #%d\n",page);
+	n_faltas_de_pagina++;
+	
+	if(!strcmp(metodo,"FIFO"))
+	{
+		//funcion metodo reemplazo fifo
+	}
+	else if (!strcmp(metodo,"rand"))
+	{
+		//funcion metodo reemplazo random
+	}
 	exit(1);
 }
 
@@ -37,14 +54,17 @@ int main( int argc, char *argv[] )
 	int npages = atoi(argv[1]);
 	int nframes = atoi(argv[2]);
 	const char *program = argv[4];
+	metodo = argv[3];
+//printf("%d \n",*metodo);
+//printf("%d \n",*program);
 	
 	frameArray *tabla_marcos = createframeArray(nframes);//guardo la tabla de marco,array simple
-	//test
+	//valoro en 0 la tabla
 	for( int i=0; i < tabla_marcos->length ; i++)
 	{
-		tabla_marcos->array[i] = i;
+		tabla_marcos->array[i] = 0;
 		printf("marco %d data %d\n",i,tabla_marcos->array[i]);
-	}//endtest
+	}
 
 	struct disk *disk = disk_open("myvirtualdisk",npages);
 	if(!disk) {
@@ -63,7 +83,7 @@ int main( int argc, char *argv[] )
 
 	char *physmem = page_table_get_physmem(pt);
 
-	page_table_set_entry(pt,npages,npages,PROT_READ|PROT_WRITE);//ACA SE CAE! <----------
+	//page_table_set_entry(pt,npages,npages,PROT_READ|PROT_WRITE);//ACA SE CAE! <----------
 
 	if(!strcmp(program,"pattern1")) {
 		access_pattern1(virtmem,npages*PAGE_SIZE);
@@ -82,6 +102,17 @@ int main( int argc, char *argv[] )
 
 	page_table_delete(pt);
 	disk_close(disk);
+	
+	free(tabla_marcos);
+	
+	//resultados
+	printf("         -----------> Input <-----------\n");
+	printf("   Algoritmo de reemplazo de pagina:   %s\n",metodo);
+	printf("   Tipo de acceso a memoria utilizado: %s\n",program);
+	printf("   Cantidad de paginas:                %d\n",npages);
+	printf("   Cantidad de marcos:                 %d\n\n",nframes);
+	printf("         -----------> Resumen <-----------\n");
+	printf("   Faltas de pagina:                   %d\n   Lecturas de disco:                  %d\n   Escrituras a disco:                 %d\n\n",n_faltas_de_pagina,n_lecturas,n_escrituras);
 
 	return 0;
 }
